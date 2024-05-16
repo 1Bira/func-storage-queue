e026c31dea1949f3bcd8c19cffd18a82f630c97c
ubiratans@agap2.pt <ubiratan.gleidson@gmail.com>
2024-05-16 13:34:45 +0100

    create function app in terraform playbook

--- tf/output.tf
+++ tf/output.tf
@@ -3,8 +3,14 @@ output "storage_account_id" {
     value = azurerm_resource_group.rg_storage_files_proc.id
 }
 
-output "sa_primary_acess_key" {
+output "sa_primary_acess_key_stg_change" {
     description = "Storage account primary acess key, created in azure"
     value = azurerm_resource_group.rg_storage_files_proc.sa_primary_acess_key
+    sensitive = true
+}
+
+output "sa_primary_acess_key_stg_func" {
+    description = "Storage account primary access key to storage fucntion"
+    value = azurerm_storage_account.ltech-stg-func-file-change.sa_primary_acess_key
     sensitive = true
 }
